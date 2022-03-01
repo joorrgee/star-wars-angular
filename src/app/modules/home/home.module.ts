bc683bb8b6f3996ccd6a4c0b5d6d15ab39d3a39a
Jorge González Pérez <jorgegonzalezperezz@gmail.com>
2022-03-01 02:59:15 +0100

    update references

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { CharacterDetailComponent } from './componets/character-detail/character-detail.component';
-import { CharacterFilmsComponent } from './componets/character-films/character-films.component';
-import { CharacterComponent } from './componets/character/character.component';
-import { PeopleComponent } from './componets/people/people.component';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
+import { CharacterFilmsComponent } from './components/character-films/character-films.component';
+import { CharacterComponent } from './components/character/character.component';
+import { PeopleComponent } from './components/people/people.component';
 import { HomeComponent } from './home.component';
 
 @NgModule({
